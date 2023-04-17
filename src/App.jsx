145803e6619542bc0958a02da2d76b0a685de4ca
Andrew Birch <andrewbirch1882@icloud.com>
2023-04-17 14:03:25 +0100

    console log id of book found

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,100 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
+// Add an ID to each book object
+const bookArray = [
+  {
+    img: "https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
+    title: "Atomic Habits",
+    author: "James Clear",
+    id: 1,
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/41X65pro2mS._SY291_BO1,204,203,200_QL40_ML2_.jpg",
+    title: "The Wasp Factory",
+    author: "Iain Banks",
+    id: 2,
+  },
+];
+
+// Create a new EventExample component here
+// and return a form
+
+const EventExample = () => {
+  // Define our event handlers here
+  const handleFormInput = (event) => {
+    console.log("onChange Fired!", event.target);
+  };
+
+  const handleClick = (event) => {
+    console.log("Button Clicked!", event.target);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("blah", e);
+  };
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <h2>Some Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={(e) => {
+            console.log(e.target.value);
+          }}
+          style={{ margin: "1rem 0" }}
+        />
+      </form>
+      <button onClick={handleSubmit}>Click Me!</button>
+    </section>
+  );
+};
+
 function App() {
+  function getBook(id) {
+    const book = bookArray.find((book) => book.id === id);
+    console.log(book);
+  }
+
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <EventExample />
+      {/* Pass the 'index' argument of the map method
+          for us to use. This is a default argument of the map method
+          and always comes second in the list of arguments. */}
+      {bookArray.map((book, index) => {
+        const { img, title, author, id } = book;
+        // Use the 'index' argument as the value for the 'key' prop.
+        return (
+          <Book
+            key={book.id} // set the key prop to book.id
+            img={img}
+            title={title}
+            author={author}
+            getBook={() => getBook(book.id)} // pass a function that calls getBook with the book's id
+          />
+        );
+      })}
     </section>
   );
 }
 
-function Book() {
+// Destructuring the props object in parentheses
+function Book({ img, title, author, children, getBook, id }) {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      {/** Use the destructured props here */}
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+
+      {/* Place your children prop here */}
+      {children}
+      <button onClick={() => getBook(id)}>Get Book</button>
     </article>
   );
 }
-
-const Image = () => <img src="https://images-eu.ssl-images-amazon.com/images/I/61e1n99gzqL._AC_UL900_SR900,600_.jpg" />;
-const Title = () => <h2>Bored of Lunch</h2>;
-const Author = () => <h4>Nathan Anthony</h4>;
 
 export default App;
